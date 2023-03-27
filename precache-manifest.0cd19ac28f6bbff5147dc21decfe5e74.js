bd9dbf216d820c7c3c0ab8b43842f8fa123ead00
SlavaOST <slavaost-it@mail.ru>
2023-03-27 19:39:35 +0300

    Updates

---
+++ precache-manifest.0cd19ac28f6bbff5147dc21decfe5e74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfcc466b92282c4c888183217e74dff3",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "c6f5916db351a8925f04",
+    "url": "/ToDoList/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "eedc512af3c91e6cc62b",
+    "url": "/ToDoList/static/js/2.317bd65b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/ToDoList/static/js/2.317bd65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6f5916db351a8925f04",
+    "url": "/ToDoList/static/js/main.19256b91.chunk.js"
+  },
+  {
+    "revision": "448f1ac8991118963dc3",
+    "url": "/ToDoList/static/js/runtime-main.97949b04.js"
+  }
+]);
